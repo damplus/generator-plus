1d06041e4348dbf10e51c08b07c076c186bacf14
Chris Devereux <chrisd(a)damplus.com>
2016-08-04 14:02:07 +0100

    Add react component generator

--- test.sh
+++ test.sh
@@ -3,17 +3,15 @@
 set -eo pipefail
 
 repo=~/Desktop/repo
-component=$repo/packages/component
 
 cd /Users/cdevereux/Tools/generator-pluskit
 
 npm run build
-rm -rf $component
-mkdir -p $repo/node_modules
+rm -rf $repo/packages
 mkdir -p $repo/packages
 
-cp -r node_modules $repo
+rm -rf $repo/node_modules/generator-pluskit
 cp -r generators $repo/node_modules/generator-pluskit
 
 cd $repo
-yo pluskit:component foo
+yo pluskit:component
